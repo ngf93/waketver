f1d8f8eeafcbb96420269518c7eb503c0b133069
Guzel <nabiullinaguzel93@gmail.com>
2024-06-30 12:21:14 +0300

    mobile

--- js/sliders.js
+++ js/sliders.js
@@ -1,7 +1,7 @@
 const swiper1 = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 4,
-    spaceBetween: 30,
+    slidesPerView: 2,
+    spaceBetween: 10,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -10,4 +10,18 @@ const swiper1 = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      }
+    }
 });
