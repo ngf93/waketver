79b729b243ee8740281d4a6524d5b1713f308804
Guzel <nabiullinaguzel93@gmail.com>
2024-06-30 04:46:32 +0300

    desktop

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/* current year */
+let now = new Date();
+let cur_year = now.getFullYear();
+document.querySelector('#year').innerHTML = cur_year;
---
+++ js/sliders.js
@@ -0,0 +1,13 @@
+const swiper1 = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
